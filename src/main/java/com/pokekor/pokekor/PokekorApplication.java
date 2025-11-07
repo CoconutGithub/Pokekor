a3a86c3633d62dc2cca782a1482e1631f28f7cc1
CoconutGithub <khkoh5932@gmail.com>
2025-11-07 09:17:23 +0900

    pokekor first commit

---
+++ src/main/java/com/pokekor/pokekor/PokekorApplication.java
@@ -0,0 +1,13 @@
+package com.pokekor.pokekor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokekorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokekorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pokekor/pokekor/PokekorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokekor.pokekor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokekorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
